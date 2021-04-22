99a6a5e398049526a28ca0d172ca5948ec484bf5
x <vinaysharma.2559@gmail.com>
2021-04-22 14:31:02 +0530

    ResuableCard widget..

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'ReusableCard.dart';
+
 class InputPage extends StatefulWidget {
   InputPage({Key? key}) : super(key: key);
 
@@ -8,20 +10,85 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  final Color activeColor = Color(0xFF1D1E33);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("BMI CALCULATOR"),
       ),
-      body: Center(
-        child: Text("TEST"),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(
+                    color: activeColor,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.male,
+                          color: Colors.white,
+                          size: 60,
+                        ),
+                        SizedBox(height: 15.0),
+                        Text(
+                          "MALE",
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            color: Color(0xFF8D8E98),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    color: activeColor,
+                    child: Container(),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ReusableCard(
+              color: activeColor,
+              child: Container(),
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(
+                    color: activeColor,
+                    child: Container(),
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    color: activeColor,
+                    child: Container(),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
 }
+
+// Container(
+//             decoration: BoxDecoration(
+//               borderRadius: BorderRadius.circular(10.0),
+//               color: Color(0xFF1D1E33),
+//             ),
+//             margin: EdgeInsets.all(15.0),
+//           ),
